3b436f4e9fd18269ce9e44bcc937c59d2091bdea
Ansah1234 <ansahpeter123@yahoo.com>
2021-04-16 15:41:37 +0100

    button styled

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import '../assets/Button.css';
 
 type buttonProps = {
 	disabled?: boolean;
+	children: React.ReactChild;
 };
 
-const Button = ({ disabled = false }: buttonProps) => {
-	return <div></div>;
+const Button = ({ children, disabled = false }: buttonProps) => {
+	const buttonStyle = {
+		backgroundColor: disabled ? 'buttonface' : 'var(--buttonColorMain)',
+	};
+	return (
+		<div className="button__box">
+			<button style={buttonStyle} disabled={disabled} className="button">
+				{children}
+			</button>
+		</div>
+	);
 };
 
 export default Button;
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,12 +5,17 @@ import Button from './Button';
 
 const Main = () => {
 	//input text field states
-	const { value: inputValue, handleChange } = useInput();   
+	const { value: inputValue, handleChange } = useInput();
 
 	return (
 		<div>
-			<input value={inputValue} onChange={handleChange} className="input" placeholder="Enter Url Here"></input>
-            <Button></Button>
+			<input
+				value={inputValue}
+				onChange={handleChange}
+				className="input"
+				placeholder="Enter Url Here"
+			></input>
+			<Button>Generate</Button>
 		</div>
 	);
 };
