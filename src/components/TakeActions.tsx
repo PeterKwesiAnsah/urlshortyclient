629645d05be858050744418d4fe66304b12a38a5
Ansah1234 <ansahpeter123@yahoo.com>
2021-04-17 08:22:23 +0100

    rendering url done

--- src/components/TakeActions.tsx
+++ src/components/TakeActions.tsx
@@ -1,10 +1,16 @@
+import '../assets/TakeActions.css';
+
 //allows user to take actions..copy or visit the shorten url
 type takeActionProp = {
 	url: string;
 };
 
-const TakeActions = ({url}:takeActionProp) => {
-	return <div></div>;
+const TakeActions = ({ url }: takeActionProp) => {
+	return (
+		<div className="takeAction">
+			<h3 className="url__text">{url}</h3>
+		</div>
+	);
 };
 
 export default TakeActions;
