bcc41691e69e66c6031057b1156d91f4909d6cb3
Ansah1234 <ansahpeter123@yahoo.com>
2021-04-21 13:55:36 +0100

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './assets/App.css';
 import Header from './components/Header';
 import Main from './components/Main';
+import Socials from './components/Socials';
 
 function App() {
 	return (
 		<>
 			<Header>Url looks too long?</Header>
 			<Main></Main>
+			<Socials></Socials>
 		</>
 	);
 }
--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import { ReactComponent as Twitter } from '../assets/twitter.svg';
+import { ReactComponent as GitHub } from '../assets/github.svg';
+import '../assets/Socials.css';
 
 const Socials = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+	return (
+		<div className="social">
+			<a href="https://twitter.com/akwesi_ansah" target="_blank">
+				<Twitter></Twitter>
+			</a>
+			<a href="https://github.com/PeterKwesiAnsah" target="_blank">
+				<GitHub></GitHub>
+			</a>
+		</div>
+	);
+};
 
-export default Socials
+export default Socials;
